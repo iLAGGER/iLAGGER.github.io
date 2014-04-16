17f6b3b2c18683a2c628d75b93d7ebdd59f6a39f
iLAGGER <ilagger@mail.ru>
2014-04-16 23:06:44 +0400

    initial comet

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+function chtoChange(combo)
+{
+
+    if(combo.value == '4')
+    {
+        document.getElementById('rashodniki').style.display = 'inline';
+        document.getElementById('buttom').style.display = 'inline';
+    }
+     else
+     {
+        document.getElementById('rashodniki').style.display = 'none';
+        document.getElementById('buttom').style.display = 'none';
+     }
+}
+
